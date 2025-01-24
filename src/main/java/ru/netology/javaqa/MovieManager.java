f29d0a55c9d4faa85b0b446393725f3065f3f388
Test <aovolkov1@gmail.com>
2025-01-24 03:18:35 +0300

    Commit 1

--- src/main/java/ru/netology/javaqa/MovieManager.java
+++ src/main/java/ru/netology/javaqa/MovieManager.java
@@ -1,5 +1,41 @@
 package ru.netology.javaqa;
 
 public class MovieManager {
+    private String[] movies = new String[0];
+    private int numberOfDisplayed;
 
+    public MovieManager(){
+        this.numberOfDisplayed=5;
+    }
+
+    public MovieManager(int numberOfDisplayed){
+        this.numberOfDisplayed=numberOfDisplayed;
+    }
+    public void addMovie(String movie) {
+        String[] tmp = new String[movies.length + 1];
+        for (int i = 0; i < movies.length; i++) {
+            tmp[i] = movies[i];
+        }
+        tmp[tmp.length - 1] = movie;
+        movies = tmp;
+    }
+
+    public String[] findAll() {
+        return movies;
+    }
+
+    public String[] findLast() {
+        int resultLength;
+        if (movies.length < numberOfDisplayed) {
+            resultLength = movies.length;
+        } else {
+            resultLength = numberOfDisplayed;
+        }
+
+        String[] last = new String[resultLength];
+        for (int i = 0; i < last.length; i++) {
+            last[i] = movies[movies.length - 1 - i]; // заполняем result из массива, что лежит в поле
+        }                                            // в обратном порядке
+        return last;
+    }
 }
---
+++ src/test/java/MovieManagerTest.java
@@ -0,0 +1,84 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import ru.netology.javaqa.MovieManager;
+
+public class MovieManagerTest {
+
+    @Test
+    public void addNothing() {
+        MovieManager manager = new MovieManager();
+        String[] expected = {};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void addOne() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Movie1");
+        String[] expected = {"Movie1"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void addSeveral() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Movie1");
+        manager.addMovie("Movie2");
+        manager.addMovie("Movie3");
+        manager.addMovie("Movie10");
+        manager.addMovie("Movie11");
+        manager.addMovie("Movie123");
+        manager.addMovie("Movie999");
+
+        String[] expected = {"Movie1","Movie2","Movie3","Movie10","Movie11","Movie123","Movie999"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void addFiveForLast() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Movie1");
+        manager.addMovie("Movie2");
+        manager.addMovie("Movie3");
+        manager.addMovie("Movie10");
+        manager.addMovie("Movie11");
+
+
+        String[] expected = {"Movie11","Movie10","Movie3","Movie2","Movie1"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void addThreeForLast() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Movie2");
+        manager.addMovie("Movie3");
+        manager.addMovie("Movie10");
+
+
+        String[] expected = {"Movie10","Movie3","Movie2"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void addSevenForLast() {
+        MovieManager manager = new MovieManager(7);
+        manager.addMovie("Movie1");
+        manager.addMovie("Movie2");
+        manager.addMovie("Movie3");
+        manager.addMovie("Movie10");
+        manager.addMovie("Movie11");
+        manager.addMovie("Movie123");
+        manager.addMovie("Movie999");
+
+        String[] expected = {"Movie999","Movie123","Movie11","Movie10","Movie3","Movie2","Movie1"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+}
